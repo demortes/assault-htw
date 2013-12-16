d7559f1dff301ed7ec4ad1656e0089d2375071ad
Kevin Dethlefs <kdethlefs@gmail.com>
2013-12-16 12:04:10 -0600

    Fixing crash.

--- src/utils.h
+++ src/utils.h
@@ -151,7 +151,7 @@ struct char_ref_type
 #define URANGE(a, b, c)         ((b) < (a) ? (a) : ((b) > (c) ? (c) : (b)))
 #define LOWER(c)                ((c) >= 'A' && (c) <= 'Z' ? (c)+'a'-'A' : (c))
 #define UPPER(c)                ((c) >= 'a' && (c) <= 'z' ? (c)+'A'-'a' : (c))
-#define IS_SET(flag, bit)       (((flag) & (bit)) == bit)
+#define IS_SET(flag, bit)       ((flag) & (bit))
 #define SET_BIT(var, bit)       ((var) = (var) | (bit))
 #define REMOVE_BIT(var, bit)    ((var) &= ~(bit))
 #define IS_LETTER(c)            ( ((c) >= 'A' && (c) <= 'Z' ) \
