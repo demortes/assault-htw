cf9e165073bc88c6a40549f65f2310413b6e8a73
Kevin Dethlefs <kdethlefs@gmail.com>
2013-12-31 21:08:34 -0600

    Atom bomber didn't modify the Z coord, so it never exploded...

--- src/fight.c
+++ src/fight.c
@@ -1835,13 +1835,14 @@ void do_blast( CHAR_DATA *ch, char *argument )
                 for ( xx = x - range; xx < x + range; xx++ )
                     for ( yy = x - range; yy < x + range; yy++ )
                     {
-                        real_coords(&xx, &yy);
-                        if ( ( bld2 = map_bld[xx][yy][ch->z] ) != NULL && bld2->active )
+                    	int tx = xx, ty = yy;
+                        real_coords(&tx, &ty);
+                        if ( ( bld2 = map_bld[tx][ty][ch->z] ) != NULL && bld2->active )
                         {
                             bld2->value[9] += bld->level * 100;
                             send_to_loc( "@@yThe building fills with nuclear fallout!!@@N\n\r", bld->x, bld->y, bld->z );
                         }
-                        for ( obj = map_obj[xx][yy]; obj; obj = obj->next_in_room )
+                        for ( obj = map_obj[tx][ty]; obj; obj = obj->next_in_room )
                         {
                             if ( obj->z != ch->z )
                                 continue;
@@ -1853,6 +1854,7 @@ void do_blast( CHAR_DATA *ch, char *argument )
             }
             ammo->x = x;
             ammo->y = y;
+            ammo->z = ch->z;
             obj_to_room(ammo, get_room_index(ROOM_VNUM_WMAP) );
             if ( number_percent() < 33 )
             {
